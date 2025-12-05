50a94ba5048bf1ac5b1a11e3f6ca00460f7d8ae5
Sebastián <spalacioalzate@gmail.com>
2025-12-04 20:41:05 -0500

    chore(django): crea aplicación paquete con modelo sin migrar

---
+++ app_backend/paquete/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Paquete(models.Model):
+
+  ESTADO_CHOICES = [
+    ("CREADO", "Creado"),
+    ("EN_TRANSITO", "En tránsito"),
+    ("ENTREGADO", "Entregado")
+  ]
+
+  nombre_destinario = models.CharField(null=False,max_length=120)
+  direccion = models.CharField(null=False,max_length=120)
+  estado = models.CharField(null=False,max_length=120,choices=ESTADO_CHOICES)
+  fecha_creacion = models.DateTimeField(null=False,auto_now_add=True)
