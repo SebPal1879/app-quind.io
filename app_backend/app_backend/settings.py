12a78f2317a6b0a83dab3a4e59ea66e8976b9429
Sebastián <spalacioalzate@gmail.com>
2025-12-04 20:47:50 -0500

    feat(db): migra paquete y migraciones por defecto

--- app_backend/app_backend/settings.py
+++ app_backend/app_backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'paquete.apps.PaqueteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
