70f20c421e687ebb7361ba8038493ecfb301d241
Sebastián <spalacioalzate@gmail.com>
2025-12-06 15:18:44 -0500

    feat(React): agrega lógica de cambio de estado en VerPaquete

--- app_frontend/src/components/VerPaquete.jsx
+++ app_frontend/src/components/VerPaquete.jsx
@@ -6,13 +6,36 @@ import axios from "axios";
 function VerPaquete() {
   const { id } = useParams();
   const [paquete, setPaquete] = useState("");
+  const [estadoForm, setEstadoForm] = useState("");
+
+  function guardarCambio() {
+    if (paquete.estado === estadoForm) return;
+  }
+
+  function validadorEstado(estado) {
+    if (paquete.estado === "CREADO" && estado !== "EN_TRANSITO") {
+      alert("Transición no válida");
+      setEstadoForm(paquete.estado);
+    } else {
+      setEstadoForm(estado);
+    }
+
+    if (paquete.estado === "EN_TRANSITO" && estado !== "ENTREGADO") {
+      alert("Transición no válida");
+      setEstadoForm(paquete.estado);
+    } else {
+      setEstadoForm(estado);
+    }
+  }
 
   useEffect(
     function () {
       async function getPaquete() {
         try {
           const respuesta = await axios.get(`${BACKEND_URL}paquetes/${id}`);
+          console.log(respuesta);
           setPaquete(respuesta.data);
+          setEstadoForm(respuesta.data.estado);
         } catch (e) {
           console.log(e);
           alert("No se encontró el paquete en cuestión");
@@ -24,22 +47,41 @@ function VerPaquete() {
   );
 
   return (
-    <table>
-      <thead>
-        <th>ID</th>
-        <th>Destinatario</th>
-        <th>Estado</th>
-        <th>Fecha creación</th>
-        <th>Acción</th>
-      </thead>
-      <tbody>
-        <td>{paquete.id}</td>
-        <td>{paquete.nombre_destinatario}</td>
-        <td>{paquete.estado}</td>
-        <td>{paquete.fecha_creacion}</td>
-        <td>Cambiar estado</td>
-      </tbody>
-    </table>
+    <div>
+      <table>
+        <thead>
+          <th>ID</th>
+          <th>Destinatario</th>
+          <th>Fecha creación</th>
+          <th>Estado</th>
+        </thead>
+        <tbody>
+          <td>{paquete.id}</td>
+          <td>{paquete.nombre_destinatario}</td>
+          <td>{paquete.fecha_creacion}</td>
+          <td>{paquete.estado}</td>
+        </tbody>
+      </table>
+
+      <label htmlFor="estados">Cambiar estado</label>
+      <select
+        name="opciones"
+        id="estados"
+        value={estadoForm}
+        onChange={(e) => validadorEstado(e.target.value)}
+      >
+        <option value="CREADO" disabled={paquete.estado === "CREADO"}>
+          Creado
+        </option>
+        <option value="EN_TRANSITO" disabled={paquete.estado === "EN_TRANSITO"}>
+          En tránsito
+        </option>
+        <option value="ENTREGADO" disabled={paquete.estado === "ENTREGADO"}>
+          Entregado
+        </option>
+      </select>
+      <button onClick={() => guardarCambio()}>Guardar</button>
+    </div>
   );
 }
 
