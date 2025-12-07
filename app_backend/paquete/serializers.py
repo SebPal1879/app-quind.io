d93a1cc497bcb66bfcd3110192cf23847c138b07
Sebastián <spalacioalzate@gmail.com>
2025-12-06 22:16:04 -0500

    feat(django): cambia la manera en la que se retorna la fecha de creación; retorna fecha y no fecha y hora

--- app_backend/paquete/serializers.py
+++ app_backend/paquete/serializers.py
@@ -4,6 +4,7 @@ from .models import Paquete
 
 class PaqueteSerializer(serializers.ModelSerializer):
   estado = serializers.SerializerMethodField()
+  fecha_creacion = serializers.SerializerMethodField()
 
   class Meta:
     model = Paquete
@@ -11,4 +12,7 @@ class PaqueteSerializer(serializers.ModelSerializer):
 
   def get_estado(self,obj):
     return obj.get_estado_display()
+  
+  def get_fecha_creacion(self,obj):
+    return obj.fecha_creacion.date()
   
