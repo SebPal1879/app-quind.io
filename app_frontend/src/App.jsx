81e2b567972145ce1dbb340d41c1892468c62c96
Sebastián <spalacioalzate@gmail.com>
2025-12-06 19:09:19 -0500

    feat(React): agrega navegación básica entre páginas

--- app_frontend/src/App.jsx
+++ app_frontend/src/App.jsx
@@ -2,13 +2,14 @@ import VerPaquete from "./components/VerPaquete";
 import VerTodosPaquetes from "./components/VerTodosPaquetes";
 import CrearPaquete from "./components/CrearPaquete";
 import Layout from "./components/Layout";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route index element={<Navigate to="paquetes" />} />
           <Route path="paquetes" element={<Layout />}>
             <Route index element={<VerTodosPaquetes />} />
             <Route path="crear" element={<CrearPaquete />} />
--- app_frontend/src/components/Layout.jsx
+++ app_frontend/src/components/Layout.jsx
@@ -1,10 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link, NavLink, useNavigate } from "react-router-dom";
 
 function Layout() {
+  const navigate = useNavigate();
   return (
     <div>
       <p>Gestión de paquetes</p>
+      <NavLink>
+        <Link to={"crear"}>Crear paquete</Link>
+        <Link to={"/paquetes"}>Ver paquetes</Link>
+      </NavLink>
       <Outlet />
+      <button onClick={() => navigate(-1)}>Retroceder</button>
     </div>
   );
 }
