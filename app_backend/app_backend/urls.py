6e85a37bca888c9a004505f27fb692a52655e11a
Sebastián <spalacioalzate@gmail.com>
2025-12-07 20:26:04 -0500

    feat(django): redirecciona desde páginas 404 a admin

--- app_backend/app_backend/urls.py
+++ app_backend/app_backend/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include, re_path
+from django.http import HttpResponseRedirect
+
+def redirect_admin(request):
+  return HttpResponseRedirect("/admin/")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('paquetes/',include("paquete.urls"))
+    path('paquetes/',include("paquete.urls")),
+    re_path(r'^.*$', redirect_admin),
 ]
